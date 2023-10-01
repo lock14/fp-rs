2198fd17e6f5ee72aa2126f00aa81cd39d34faa7
Brian Bechtel <lock14@users.noreply.github.com>
2023-10-01 13:48:49 -0700

    Change Magma to extend std::ops::Add (#3)
    * try to get operator overloading to work
    
    * Change Magma to exend std::ops::Add
    
    * Change the Magma trait to be an extension of `std::ops::Add` where the type
      bounds are restricted such that the add operation is closed under type `T`.
    
    * add documentation for Magma trait
    
    * tweak documentation

--- src/magma.rs
+++ src/magma.rs
@@ -1,6 +1,9 @@
-pub trait Magma<T> {
-    fn add(&self, other: &T) -> T;
-}
+use std::ops::{Add};
+
+/// A Magma defines a binary operation 'add' (denoted hereafter by `+`)
+/// over type `T` with the following properties:
+/// * `+` is closed over type `T`
+pub trait Magma<T>: Add<T, Output=T> {}
 
 #[cfg(test)]
 mod tests {
@@ -8,14 +11,37 @@ mod tests {
 
     // implement Magma for type i32
     impl Magma<i32> for i32 {
-        fn add(&self, other: &i32) -> i32 {
-            return self + other;
+
+    }
+
+    // define custom struct and implement Magma
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    struct Foo {
+        x: i32,
+    }
+
+    impl Add for Foo {
+        type Output = Foo;
+
+        fn add(self, rhs: Foo) -> Foo {
+            return Foo{x: self.x + rhs.x}
         }
+    }
+
+    impl Magma<Foo> for Foo {
     }
 
     #[test]
     fn i32_is_a_magma() {
         let x: i32 = 2;
         assert_eq!(x.add(&2), 4);
+    }
+
+    #[test]
+    fn foo_is_magma_with_operator_overloading() {
+        let foo_x = Foo { x: 2 };
+        let foo_y = Foo { x: 3 };
+        assert_eq!(foo_x + foo_y, Foo { x: 5 });
+        assert_eq!(foo_x.add(foo_y), Foo { x: 5 });
     }
 }
